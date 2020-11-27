2ea5687c5e3427d5af982adba324854ce3db9d97
Ederson Faccin Frasson <edersonfrasson@gmail.com>
2020-11-27 15:48:45 -0300

    Login google android - em andamento

--- App.js
+++ App.js
@@ -1,114 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+//import auth from '@react-native-firebase/auth'
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin'
 
 const App = () => {
+
+  const [ userInfo, setUserInfo ] = useState(null)
+  const [ user, setUser ] = useState(null)
+  const [ isLoginScreenPresented, setIsLoginScreenPresented ] = useState(null)
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '864806810263-od33v15709ua29kpc89t4glk4cb3mg0l.apps.googleusercontent.com'
+    })
+  }, [])
+
+  const signIn = async () => {
+    var retorno = await GoogleSignin.signIn()
+    console.log(retorno)
+    /*try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      
+      console.log(userInfo)
+     
+      setUserInfo(userInfo)
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('statusCodes.SIGN_IN_CANCELLED', statusCodes.SIGN_IN_CANCELLED)
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+        console.log('statusCodes.IN_PROGRESS', statusCodes.IN_PROGRESS)
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log('statusCodes.PLAY_SERVICES_NOT_AVAILABLE', statusCodes.PLAY_SERVICES_NOT_AVAILABLE)
+      } else {
+        // some other error happened
+        console.log('error', error)
+      }
+    }*/
+  }
+
+  const isSignedIn = async() => {
+    const isSignedIn = await GoogleSignin.isSignedIn()
+
+    setIsLoginScreenPresented(!isSignedIn)
+  }
+
+  const signOut = async() => {
+    try {
+      await GoogleSignin.revokeAccess()
+      await GoogleSignin.signOut()
+      
+      setUser(null)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <View style={styles.body}>
+      <Text>Login Social using Firebase</Text>
+
+      <GoogleSigninButton
+        style={{ width: 192, height: 48 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+        // disabled={this.state.isSigninInProgress}
+         />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    backgroundColor: '#ADD8E6',
+    flex: 1,
+    alignItems: 'center'
+  }  
+})
 
-export default App;
+export default App
